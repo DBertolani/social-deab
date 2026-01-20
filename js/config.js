2e2c7b71faf7d4e757ead2c03da6e33c9e971c4b
DBertolani <104177844+DBertolani@users.noreply.github.com>
2026-01-20 15:18:50 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 const CONFIG = {
     // Sua URL do Google Apps Script (ATUALIZE SEMPRE AQUI)
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzRULFJ7Lr3QwfQJnkILtt53IMtOYKtalsWF-91-a2LpCm-KSXJNgQMVe20BbTPNCTaIQ/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwePrOqUhcq6m4GPlrBi5MQdYwcZt6NLD0dyL_Yd6bofbdRaXHdMtsvIZVxXkLIYbnnMA/exec",
     
     // Outras configs globais podem vir aqui no futuro
     MOEDA: "BRL"
