4d49a00596db6d48969f11fd2fb0878f5b7b77a8
DBertolani <104177844+DBertolani@users.noreply.github.com>
2026-01-20 21:32:33 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 // js/config.js
-const CONFIG = {
-    // Sua URL do Google Apps Script (ATUALIZE SEMPRE AQUI)
+// Configuração global blindada contra múltiplos loads
+
+window.CONFIG = window.CONFIG || {
+    // URL do Google Apps Script
     SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwePrOqUhcq6m4GPlrBi5MQdYwcZt6NLD0dyL_Yd6bofbdRaXHdMtsvIZVxXkLIYbnnMA/exec",
     
     // Outras configs globais podem vir aqui no futuro
